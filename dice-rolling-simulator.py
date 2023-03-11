134ae47cfb8d0555ff03ed92657a6f7e55c6e1f5
StarryGhost <85236486+StarryGhost@users.noreply.github.com>
2023-03-10 20:40:57 -0500

    Update dice-rolling-simulator.py

--- dice-rolling-simulator.py
+++ dice-rolling-simulator.py
@@ -4,20 +4,15 @@ import random
 #range of the values of a dice
 min_val = 1
 max_val = 6
+dice
 
-#to loop the rolling through user input
-roll_again = "yes"
 
 #loop
-while roll_again == "yes" or roll_again == "y":
+while max_val != 2:
     print("Rolling The Dices...")
     print("The Values are :")
     
     #generating and printing 1st random integer from 1 to 6
-    print(random.randint(min_val, max_val))
+    dice = random.randint(min_val, max_val)
+    print(dice)
     
-    #generating and printing 2nd random integer from 1 to 6
-    print(random.randint(min_val, max_val))
-    
-    #asking user to roll the dice again. Any input other than yes or y will terminate the loop
-    roll_again = input("Roll the Dices Again?") 
